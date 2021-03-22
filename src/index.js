cac1e45e3c17b22a681783fc44228e62c9f07317
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-03-22 17:47:13 +0300

    fix(prefix-detection): more sensitive now
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/index.js
+++ src/index.js
@@ -115,111 +115,122 @@ const run = async () => {
         await setActivity()
     })
 
-    // SSS Message Find Callback
+    // Prefix Detect Callback
     client.on('message', async message => {
         const content = message.content
-        if(content.startsWith(Prefixes.SSS)){ // SSS
-            const search = Object.keys(QuestionMap).filter(key => {
-                const regex = new RegExp(`${content.substr(Prefixes.SSS.length, content.length).trim().toLowerCase()}`)
+        const args = content.split(' ')
+        const arg0 = args[0]?.trim()
+        switch(arg0){
+            case Prefixes.SSS:
+                const search = Object.keys(QuestionMap).filter(key => {
+                    const regex = new RegExp(`${content.substr(Prefixes.SSS.length, content.length).trim().toLowerCase()}`)
 
-                return regex.test(key)
-            })
+                    return regex.test(key)
+                })
 
-            if(search.length !== 0){
-                const question_data = QuestionMap[search.shift()]
-                if(question_data && message.channel.type === 'text'){
+                if(search.length !== 0){
+                    const question_data = QuestionMap[search.shift()]
+                    if(question_data && message.channel.type === 'text'){
+                        await Promise.all([
+                            message.channel.send([
+                                `Q: **${question_data.Q}**`,
+                                `A: ${question_data.A}`
+                            ]),
+                            message.delete({ timeout: 1 })
+                        ])
+                    }
+                }
+                break
+
+            case 'top':
+                db.all('SELECT * FROM votes ORDER BY count DESC LIMIT 10', (err, rows) => {
+                    let text = ''
+                    if(rows.length === 0){
+                        text = [
+                            'Henüz kimse oy vermemiş.',
+                            `[İlk veren olmak için tıkla.](${URLMap.TOP_GG_VOTE_URL})`
+                        ]
+                    }else{
+                        let i = 1
+                        for(const row of rows){
+                            text += message.author.id === row.user_id
+                                ?
+                                `**#${i} | <@${row.user_id}> Oy: \`${row.count}\`\n**`
+                                :
+                                `#${i} | <@${row.user_id}> Oy: \`${row.count}\`\n`
+                            i++
+                        }
+                    }
+
+                    const embed = new MessageEmbed()
+                        .setAuthor('📋 Sunucu Oy Sıralaması', message.author.avatarURL() || message.author.defaultAvatarURL)
+                        .setTimestamp()
+                        .setColor('GOLD')
+                        .addField('En Çok Oy Veren Kullanıcılar', text)
+
+                    message.channel.send({ embed })
+                })
+                break
+
+            case Prefixes.Wiki:
+                const cmd = args[1]
+                let url = URLMap.WIKI_URL
+                if(WikiMap.Commands.includes(cmd)){
+                    url += `/docs/commands/${cmd}`
+                }
+
+                await Promise.all([
+                    message.channel.send(url),
+                    message.delete()
+                ])
+                break
+
+            default:
+                if(!content.startsWith(Prefixes.Shortcuts)) break
+
+                let text, options
+                switch(content.substr(Prefixes.Shortcuts.length, content.length).trim().toLowerCase()){
+                    case 'davet':
+                    case 'invite':
+                        text = '**Sunucu Davet:** https://discord.gg/CRgXhfs'
+                        break
+
+                    case 'website':
+                    case 'site':
+                        text = '**Asena Website**: https://asena.xyz'
+                        break
+
+                    case 'kod':
+                    case 'code':
+                        text = 'Kod paylaşım bloğu nasıl yapılır?'
+                        options = {
+                            files: [
+                                'https://cdn.discordapp.com/attachments/729930836857716747/765936916952383499/lang.PNG'
+                            ]
+                        }
+                        break
+
+                    case 'github':
+                    case 'star':
+                        text = [
+                            '**Asena** \'nın kaynak kodlarına ulaşmak için: https://github.com/anilmisirlioglu/Asena',
+                            '**NOT:** Sağ üst köşeden projeye `star` (:star:) vermeyi unutmayın <:AsenaLogo:764464729283493908>'
+                        ]
+                        break
+
+                    case 'vote':
+                    case 'oy':
+                        text = 'Botumuzu destekleyip oy vermek için: https://top.gg/bot/716259870910840832'
+                        break
+                }
+
+                if(text){
                     await Promise.all([
-                        message.channel.send([
-                            `Q: **${question_data.Q}**`,
-                            `A: ${question_data.A}`
-                        ]),
+                        message.channel.send(text, options),
                         message.delete({ timeout: 1 })
                     ])
                 }
-            }
-        }else if(message.content === 'top'){ // Top Voters
-            db.all('SELECT * FROM votes ORDER BY count DESC LIMIT 10', (err, rows) => {
-                let text = ''
-                if(rows.length === 0){
-                    text = [
-                        'Henüz kimse oy vermemiş.',
-                        `[İlk veren olmak için tıkla.](${URLMap.TOP_GG_VOTE_URL})`
-                    ]
-                }else{
-                    let i = 1
-                    for(const row of rows){
-                        text += message.author.id === row.user_id
-                            ?
-                            `**#${i} | <@${row.user_id}> Oy: \`${row.count}\`\n**`
-                            :
-                            `#${i} | <@${row.user_id}> Oy: \`${row.count}\`\n`
-                        i++
-                    }
-                }
-
-                const embed = new MessageEmbed()
-                    .setAuthor('📋 Sunucu Oy Sıralaması', message.author.avatarURL() || message.author.defaultAvatarURL)
-                    .setTimestamp()
-                    .setColor('GOLD')
-                    .addField('En Çok Oy Veren Kullanıcılar', text)
-
-                message.channel.send({ embed })
-            })
-        }else if(content.startsWith(Prefixes.Wiki)){ // Wiki
-            const args = content.split(' ')
-            const cmd = args[1]
-            let url = URLMap.WIKI_URL
-            if(WikiMap.Commands.includes(cmd)){
-                url += `/docs/commands/${cmd}`
-            }
-
-            await Promise.all([
-                message.channel.send(url),
-                message.delete()
-            ])
-        }else if(content.startsWith(Prefixes.Shortcuts)){ // Custom Commands
-            let text, options
-            switch(content.substr(Prefixes.Shortcuts.length, content.length).trim().toLowerCase()){
-                case 'davet':
-                case 'invite':
-                    text = '**Sunucu Davet:** https://discord.gg/CRgXhfs'
-                    break
-
-                case 'website':
-                case 'site':
-                    text = '**Asena Website**: https://asena.xyz'
-                    break
-
-                case 'kod':
-                case 'code':
-                    text = 'Kod paylaşım bloğu nasıl yapılır?'
-                    options = {
-                        files: [
-                            'https://cdn.discordapp.com/attachments/729930836857716747/765936916952383499/lang.PNG'
-                        ]
-                    }
-                    break
-
-                case 'github':
-                case 'star':
-                    text = [
-                        '**Asena** \'nın kaynak kodlarına ulaşmak için: https://github.com/anilmisirlioglu/Asena',
-                        '**NOT:** Sağ üst köşeden projeye `star` (:star:) vermeyi unutmayın <:AsenaLogo:764464729283493908>'
-                    ]
-                    break
-
-                case 'vote':
-                case 'oy':
-                    text = 'Botumuzu destekleyip oy vermek için: https://top.gg/bot/716259870910840832'
-                    break
-            }
-
-            if(text){
-                await Promise.all([
-                    message.channel.send(text, options),
-                    message.delete({ timeout: 1 })
-                ])
-            }
+                break
         }
     })
 }
