cecb089303066fdd083b86c103458a331cd3eb59
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-08-20 23:18:42 +0300

    feat: added configuration for access to remote redis server
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/drivers/Redis.js
+++ src/drivers/Redis.js
@@ -1,10 +1,17 @@
 const redis = require('redis')
-const publisher = redis.createClient()
+const createRedisClient = () => {
+    return redis.createClient({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD
+    })
+}
 
+const publisher = createRedisClient()
 publisher.on('connect', () => console.log('Yayımcı, Redis veritabanına bağlandı.'))
 publisher.config('SET', 'notify-keyspace-events', 'Kx')
-const subscriber = redis.createClient()
 
+const subscriber = createRedisClient()
 subscriber.on('connect', () => console.log('Abone, Redis veritabanına bağlandı.'))
 subscriber.on('disconnect', () => console.log('Abonenin Redis veritabanı ile bağlantısı kesildi.'))
 
